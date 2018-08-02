91321b319829701f9f0b80ea15e472dc8163cb90
davildws <dwsandroid@163.com>
2018-08-02 11:31:03 +0800

    fix bug

---
+++ config/src/main/java/org/study/spring/cloud/config/ConfigApplication.java
@@ -0,0 +1,16 @@
+package org.study.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dongws
+ * @date 2018/8/2
+ */
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class,args);
+    }
+}
